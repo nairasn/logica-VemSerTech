50198f2a82da63511c79e0f3eb401159448ea7a4
Naira Souza <87916977+nairasn@users.noreply.github.com>
2023-11-24 23:39:46 -0300

    Add files via upload

---
+++ projeto-final-logica-JS/todo.js
@@ -0,0 +1,47 @@
+const tarefas = [];
+
+function adicionar_tarefa(id, titulo, descricao) {
+    
+    if 
+
+
+    tarefa.push({ id, titulo, descricao });
+    console.log("Tarefa adicionada com sucesso!");
+}
+
+
+function editar_tarefa(id, novoTitulo, novaDescricao) {
+    
+
+
+
+    console.log("Tarefa editada com sucesso!");
+}
+
+
+function remover_tarefa(id) {
+
+    
+    if ()
+
+
+    console.log("Tarefa removida com sucesso!");
+}
+
+
+function listar_tarefas() {
+    
+}
+
+
+function obter_tarefaP_ID(id) {
+    
+    
+
+    
+    if ()
+
+    // Retornar a tarefa encontrada
+    
+}
+
